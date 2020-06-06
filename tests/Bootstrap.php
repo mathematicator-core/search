a59630ff5ea3260adb2d0ee6c474c59a984551d0
Miloš Havlíček <miloshavlicek@gmail.com>
2020-06-06 14:00:50 +0200

    Tests autoloader only if available

--- tests/Bootstrap.php
+++ tests/Bootstrap.php
@@ -5,14 +5,15 @@ declare(strict_types=1);
 namespace Mathematicator\Search\Tests;
 
 
-require __DIR__ . '/../vendor/autoload.php';
-
 use Mathematicator\Engine\Translation\TranslatorHelper;
 use Nette\Configurator;
 use Nette\DI\Container;
 use Tester\Environment;
 
-Environment::setup();
+if (\is_file($autoload = __DIR__ . '/../vendor/autoload.php')) {
+	require_once $autoload;
+	Environment::setup();
+}
 
 class Bootstrap
 {
